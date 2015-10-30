88926e2847669f6c59acc08f0011e8e8abbdf9a2
mbattur <mbattur@yahoo.com>
2015-10-30 19:48:23 +0000

    assignment 40 completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,6 +36,10 @@ RSpec.describe User, type: :model do
      it "should respond to member?" do
          expect(user).to respond_to(:member?)
      end
+     
+     it "should respond to moderator" do
+         expect(user).to respond_to(:moderator?)
+     end
    end
    
    describe "roles" do
@@ -53,6 +57,21 @@ RSpec.describe User, type: :model do
            expect(user.admin?).to be_falsey 
         end
      end
+     
+     context "moderator" do
+        it "should return true for #moderator?" do
+           expect(user.moderator).to be_truthy
+        end
+        
+        it "should return false for #admin?" do
+           expect(user.admin).to be_falsey
+        end
+        
+        it "should return false for #member?" do
+           expect(user.member).to be_falsey
+        end
+     end
+   end
      
      context "admin user" do
         before do
