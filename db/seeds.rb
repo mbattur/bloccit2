a40bfb99e9b6aacc7799284ea99da73c87002429
mbattur <mbattur@yahoo.com>
2015-11-04 20:45:36 +0000

    checkpoing 43 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,14 @@ include RandomData
  topics = Topic.all
 
  50.times do
-   Post.create!(
+   post = Post.create!(
      user: users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: user.sample) }
  end
  
  50.times do
@@ -64,3 +66,4 @@ include RandomData
  puts "#{Post.count} posts created"
  puts "#{SponsoredPost.count} sponsored post created"
  puts "#{Comment.count} comments created"
+ puts "#{Vote.count} votes created"
