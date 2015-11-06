911a432f018629b825e8f3123988682cca9208b5
mbattur <mbattur@yahoo.com>
2015-11-06 15:09:32 +0000

    assignment 44 completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -13,4 +13,15 @@ class FavoriteMailer < ActionMailer::Base
 
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+   
+   def new_post(post)
+
+     headers["Message-ID"] = "<comments/#{comment.id}@aqueous-fortress-4895.herokuapp.com>"
+     headers["In-Reply-To"] = "<post/#{post.id}@aqueous-fortress-4895.herokuapp.com>"
+     headers["References"] = "<post/#{post.id}@aqueous-fortress-4895.herokuapp.com>"
+ 
+     @post = post
+
+     mail(to: post.user.email, subject: "You're following, #{post.title}!")
+   end
 end
