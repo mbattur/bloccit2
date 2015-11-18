0722bf81235670b0eefa145a1b2d55dfdb2db934
mbattur <mbattur@yahoo.com>
2015-11-18 15:12:58 +0000

    assignment 42 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
     belongs_to :topic
     belongs_to :user
-    belongs_to :rating
     has_many :comments, dependent: :destroy
     has_many :labelings, as: :labelable
     has_many :labels, through: :labelings
+    has_one :rating
     
     
     default_scope { order('created_at DESC') }
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-it { should have_many :topics }
-it { should have_many :posts }
-
 RSpec.describe Rating, type: :model do
-
-   
+  describe ".update_rating" do
+    it "creates a non-existent rating" do
+      expect(Rating.update_rating("PG")).to_not be_nil
+    end
+  end
 end
